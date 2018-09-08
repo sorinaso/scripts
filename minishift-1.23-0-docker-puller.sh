f537e2042399317924475829675a340208159a54
sorinaso <sorinaso@gmail.com>
2018-09-08 16:46:43 -0300

    Update minishift-1.23-0-docker-puller.sh

--- minishift-1.23-0-docker-puller.sh
+++ minishift-1.23-0-docker-puller.sh
@@ -3,4 +3,9 @@ REPEAT_TIMES=${REPEAT_TIMES?"Se debe definir REPEAT_TIMES ejemplo: 10"}
 
 echo "Repitiendo ${REPEAT_TIMES} veces con timeout de ${PULL_TIMEOUT}"
 
-for d in $(seq 1 $REPEAT_TIMES); do timeout $PULL_TIMEOUT docker pull openshift/origin-control-plane:v3.10; done;
+for d in $(seq 1 $REPEAT_TIMES) 
+do 
+  echo "Repeticion numero ${d}:"
+  
+  timeout $PULL_TIMEOUT docker pull openshift/origin-control-plane:v3.10 
+done
