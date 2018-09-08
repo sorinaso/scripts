0466487886a5ae2fa89a1baa4b8e2cbc0a54cc1f
sorinaso <sorinaso@gmail.com>
2018-09-08 16:22:06 -0300

    Informacion de repeticion y timeout

--- minishift-1.23-0-docker-puller.sh
+++ minishift-1.23-0-docker-puller.sh
@@ -1,4 +1,6 @@
 PULL_TIMEOUT=60s
 REPEAT_TIMES=10
 
+echo "Repitiendo ${REPEAT_TIMES} veces con timeout de ${PULL_TIMEOUT}"
+
 for d in $(seq 1 $REPEAT_TIMES); do timeout $PULL_TIMEOUT docker pull openshift/origin-control-plane:v3.10; done;
