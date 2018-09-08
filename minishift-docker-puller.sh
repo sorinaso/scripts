dc4c84bfc48f89a79b347a50e51625dd8c7a3f3b
sorinaso <sorinaso@gmail.com>
2018-09-08 17:50:45 -0300

    Agregada version de minishift

---
+++ minishift-docker-puller.sh
@@ -0,0 +1,27 @@
+# Para correrlo bash <(curl -s http://mywebsite.com/myscript.txt)
+
+PULL_TIMEOUT=${PULL_TIMEOUT?"Se debe definir PULL_TIMEOUT ejemplo: 60s"}
+REPEAT_TIMES=${REPEAT_TIMES?"Se debe definir REPEAT_TIMES ejemplo: 10"}
+MINISHIFT_VERSION=${MINISHIFT_VERSION?"Se debe definir MINISHIFT_VERSION ejemplo: 1.23.0"}
+
+echo "Repitiendo ${REPEAT_TIMES} veces con timeout de ${PULL_TIMEOUT}"
+
+for d in $(seq 1 $REPEAT_TIMES)
+do 
+  echo "Repeticion numero ${d}:"
+  
+  case $MINISHIFT_VERSION in
+  1.23.0)
+    timeout $PULL_TIMEOUT docker pull openshift/origin:v3.9.0
+    #timeout $PULL_TIMEOUT docker pull openshift/origin-control-plane:v3.10 
+    #timeout $PULL_TIMEOUT docker pull openshift/origin-cli:v3.10 
+  1.23.0)
+    timeout $PULL_TIMEOUT docker pull openshift/origin:v3.10.0
+    timeout $PULL_TIMEOUT docker pull openshift/origin-control-plane:v3.10 
+    timeout $PULL_TIMEOUT docker pull openshift/origin-cli:v3.10 
+    ;;
+  *)
+    echo "Version de Minishift ${MINISHIFT_VERSION} no manejada."      
+    ;;
+  esac
+done
