17645f888246083f54ace9efffe42ac4e3a01a9d
Alejandro Souto <alesouto.it@gmail.com>
2018-09-08 17:58:28 -0300

    fix en script

--- minishift-docker-puller.sh
+++ minishift-docker-puller.sh
@@ -1,4 +1,4 @@
-# Para correrlo bash: 
+# Para correrlo bash:
 # PULL_TIMEOUT=120s REPEAT_TIMES=20 MINISHIFT_VERSION=1.23.0 bash <(curl -s https://github.com/sorinaso/scripts/raw/master/minishift-docker-puller.sh)
 
 PULL_TIMEOUT=${PULL_TIMEOUT?"Se debe definir PULL_TIMEOUT ejemplo: 60s"}
@@ -8,21 +8,22 @@ MINISHIFT_VERSION=${MINISHIFT_VERSION?"Se debe definir MINISHIFT_VERSION ejemplo
 echo "Repitiendo ${REPEAT_TIMES} veces con timeout de ${PULL_TIMEOUT}"
 
 for d in $(seq 1 $REPEAT_TIMES)
-do 
+do
   echo "Repeticion numero ${d}:"
-  
+
   case $MINISHIFT_VERSION in
-  1.23.0)
+  1.22.0)
     timeout $PULL_TIMEOUT docker pull openshift/origin:v3.9.0
-    #timeout $PULL_TIMEOUT docker pull openshift/origin-control-plane:v3.10 
-    #timeout $PULL_TIMEOUT docker pull openshift/origin-cli:v3.10 
+    #timeout $PULL_TIMEOUT docker pull openshift/origin-control-plane:v3.10
+    #timeout $PULL_TIMEOUT docker pull openshift/origin-cli:v3.10
+    ;;
   1.23.0)
     timeout $PULL_TIMEOUT docker pull openshift/origin:v3.10.0
-    timeout $PULL_TIMEOUT docker pull openshift/origin-control-plane:v3.10 
-    timeout $PULL_TIMEOUT docker pull openshift/origin-cli:v3.10 
+    timeout $PULL_TIMEOUT docker pull openshift/origin-control-plane:v3.10
+    timeout $PULL_TIMEOUT docker pull openshift/origin-cli:v3.10
     ;;
   *)
-    echo "Version de Minishift ${MINISHIFT_VERSION} no manejada."      
+    echo "Version de Minishift ${MINISHIFT_VERSION} no manejada."
     ;;
   esac
 done
