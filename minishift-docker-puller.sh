4c14c5f34558640ecda423ed96e9829f1232fb06
sorinaso <sorinaso@gmail.com>
2018-09-08 21:07:25 -0300

    Update minishift-docker-puller.sh

--- minishift-docker-puller.sh
+++ minishift-docker-puller.sh
@@ -4,6 +4,10 @@ MINISHIFT_VERSION=${MINISHIFT_VERSION?"Se debe definir MINISHIFT_VERSION ejemplo
 
 echo "Repitiendo ${REPEAT_TIMES} veces con timeout de ${PULL_TIMEOUT}"
 
+function pull_docker_image() {
+  (docker inspect --type=image $1 2&> /dev/null) || (timeout $PULL_TIMEOUT docker pull $1)
+}
+
 for d in $(seq 1 $REPEAT_TIMES)
 do
   trap "exit 1;" SIGHUP SIGINT SIGTERM
@@ -20,7 +24,8 @@ do
     #timeout $PULL_TIMEOUT docker pull openshift/origin:v3.10.0
     #timeout $PULL_TIMEOUT docker pull openshift/origin-control-plane:v3.10
     #timeout $PULL_TIMEOUT docker pull openshift/origin-cli:v3.10
-    timeout $PULL_TIMEOUT docker pull openshift/origin-node:v3.10
+    #timeout $PULL_TIMEOUT docker pull openshift/origin-node:v3.10
+    pull_docer_image "openshift/origin-node:v3.10"
     ;;
   *)
     echo "Version de Minishift ${MINISHIFT_VERSION} no manejada."
