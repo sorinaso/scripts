72e4191f81f4cff574b04ab335f342bd3277b21a
Alejandro Souto <alesouto.it@gmail.com>
2018-09-08 21:31:23 -0300

    Pull si se requiere

--- minishift-docker-puller.sh
+++ minishift-docker-puller.sh
@@ -1,17 +1,21 @@
+#!/usr/bin/env bash
+
 PULL_TIMEOUT=${PULL_TIMEOUT?"Se debe definir PULL_TIMEOUT ejemplo: 60s"}
 REPEAT_TIMES=${REPEAT_TIMES?"Se debe definir REPEAT_TIMES ejemplo: 10"}
 MINISHIFT_VERSION=${MINISHIFT_VERSION?"Se debe definir MINISHIFT_VERSION ejemplo: 1.23.0"}
 
 echo "Repitiendo ${REPEAT_TIMES} veces con timeout de ${PULL_TIMEOUT}"
 
-function pull_docker_image() {
-  (docker inspect --type=image $1 2&> /dev/null) || (timeout $PULL_TIMEOUT docker pull $1)
+set -xe
+
+function get_docker_image () {
+  (docker inspect --type=image $1 > /dev/null) && echo "La imagen $1 existe." || (timeout $PULL_TIMEOUT docker pull $1)
 }
 
 for d in $(seq 1 $REPEAT_TIMES)
 do
   trap "exit 1;" SIGHUP SIGINT SIGTERM
-  
+
   echo "Repeticion numero ${d}:"
 
   case $MINISHIFT_VERSION in
@@ -21,11 +25,11 @@ do
     #timeout $PULL_TIMEOUT docker pull openshift/origin-cli:v3.10
     ;;
   1.23.0)
-    #timeout $PULL_TIMEOUT docker pull openshift/origin:v3.10.0
-    #timeout $PULL_TIMEOUT docker pull openshift/origin-control-plane:v3.10
-    #timeout $PULL_TIMEOUT docker pull openshift/origin-cli:v3.10
-    #timeout $PULL_TIMEOUT docker pull openshift/origin-node:v3.10
-    pull_docer_image "openshift/origin-node:v3.10"
+    get_docker_image "openshift/origin:v3.10.0"
+    get_docker_image "openshift/origin-control-plane:v3.10"
+    get_docker_image "openshift/origin-cli:v3.10"
+    get_docker_image "openshift/origin-node:v3.10"
+    get_docker_image "openshift/origin-node:v3.10"
     ;;
   *)
     echo "Version de Minishift ${MINISHIFT_VERSION} no manejada."
